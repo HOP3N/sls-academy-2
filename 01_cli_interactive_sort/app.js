b247d2cd548316aff45990a8fcd7bb23dd2ace95
Mykola Pankov <pankoff97@gmail.com>
2023-06-09 19:39:33 +0300

    cntnd chngs

--- 01_cli_interactive_sort/app.js
+++ 01_cli_interactive_sort/app.js
@@ -6,52 +6,52 @@ const rl = readline.createInterface({
 });
 
 const sortWordsAtoZ = (words) => [...words].sort();
-
 const sortNumsAscending = (numbers) => [...numbers].sort((a, b) => a - b);
-
 const sortNumsDescending = (numbers) => [...numbers].sort((a, b) => b - a);
-
 const sortWordsLengthAscending = (words) =>
   [...words].sort((a, b) => a.length - b.length);
-
 const getOnlyUniqueWords = (words) => [...new Set(words)];
-
 const getOnlyUniqueValues = (values) => [...new Set(values)];
 
 const processInput = (input) => {
   const words = input.split(' ');
-  const operations = {
-    1: sortWordsAtoZ,
-    2: sortNumsAscending,
-    3: sortNumsDescending,
-    4: sortWordsLengthAscending,
-    5: getOnlyUniqueWords,
-    6: getOnlyUniqueValues,
+
+  const handleChoice = (choice) => {
+    const operations = {
+      1: sortWordsAtoZ,
+      2: sortNumsAscending,
+      3: sortNumsDescending,
+      4: sortWordsLengthAscending,
+      5: getOnlyUniqueWords,
+      6: getOnlyUniqueValues,
+    };
+
+    const selectedOperation = operations[choice];
+
+    if (selectedOperation) {
+      const result = selectedOperation(words);
+      console.log('Result:', result.join(' '));
+    } else {
+      console.log('Invalid choice!');
+      rl.close();
+      return;
+    }
+
+    rl.question(
+      'Enter "exit" to exit or press enter to continue: ',
+      (answer) => {
+        if (answer === 'exit') {
+          rl.close();
+        } else {
+          rl.prompt();
+        }
+      }
+    );
   };
 
-  const choice = Number(input);
-  const operation = operations[choice];
-
-  if (operation) {
-    const result = operation(words);
-    console.log('Result:', result.join(' '));
-  } else {
-    console.log('Invalid choice!');
-  }
-
-  rl.question('Enter "exit" to exit or press enter to continue: ', (answer) => {
-    if (answer === 'exit') {
-      rl.close();
-    } else {
-      rl.prompt();
-    }
-  });
-};
-
-rl.question(
-  `
-What would You like to to see in the output - what operation to do with words and numbers, namely:
-
+  rl.question(
+    `
+What would you like to see in the output? (Enter the corresponding number)
 1. Sort words alphabetically
 2. Show numbers from lesser to greater
 3. Show numbers from bigger to smaller
@@ -59,8 +59,23 @@ What would You like to to see in the output - what operation to do with words an
 5. Show only unique words
 6. Display only unique values
 
-Enter your choice: Your number`,
-  (input) => {
+Enter your choice: `,
+    handleChoice
+  );
+};
+
+rl.prompt();
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    rl.close();
+  } else {
     processInput(input);
   }
-);
+});
+
+rl.on('close', () => {
+  console.log('Exiting the program...');
+  process.exit(0);
+});
+
+console.log('Enter words or numbers separated by spaces:');

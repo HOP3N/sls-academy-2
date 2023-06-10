48744b02e80f12545ef43f60ac6fc48ae07ea6bc
Mykola Pankov <pankoff97@gmail.com>
2023-06-10 21:02:21 +0300

    fncts bgn cd

--- 02_cli_primitive_db/appDb.js
+++ 02_cli_primitive_db/appDb.js
@@ -2,3 +2,68 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 const dbPath = 'users.txt';
+
+function createUser() {
+  inquirer
+    .createPromptModule([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter a name (Press ENTER to finish):',
+        validate: (input) => input.trim() !== '' || 'Name is required',
+      },
+      {
+        type: 'list',
+        name: 'gender',
+        message: 'Please select a gender:',
+        choices: ['Male', 'Female', 'Other'],
+        when: (answers) => answers.name.trim() !== '',
+      },
+      {
+        type: 'input',
+        name: 'age',
+        message: 'Please enter the age:',
+        when: (answers) => answers.name.trim() !== '',
+      },
+    ])
+    .then((answers) => {
+
+      if (answers.name.trim() !== '') {
+        const user = {
+          name: answers.name,
+          gender: answers.gender,
+          age: answers.age,
+        };
+
+        saveUser(user);
+        console.log('User information saved successfully!');
+
+        createUser();
+
+      } else {
+        findUser();
+      }
+    });
+}
+
+function saveUser(user) {
+  const users = getUsers();
+  users.push(user);
+  fs.writeFileSync(dbPath, JSON.stringify(users));
+}
+
+function getUsers() {
+
+  try {
+    const data = fs.readFileSync(dbPath, 'utf8');
+    return JSON.parse(data);
+
+  } catch (error) {
+    console.log(`Error reading database: ${error.message}`)
+    return [];
+  }
+}
+
+function findUser() {
+  
+}

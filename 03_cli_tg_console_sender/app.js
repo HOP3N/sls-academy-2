a12b32e60b611c130b05bd90cfdaf886e76c7ede
Mykola Pankov <pankoff97@gmail.com>
2023-06-11 18:39:53 +0300

    tgBtTkn

---
+++ 03_cli_tg_console_sender/app.js
@@ -0,0 +1,5 @@
+const commander = require('commander');
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot('5615659005:AAGBL7SqnLeZsiwSWti46EnpbyGbG_8gjsg', {
+  polling: false,
+});

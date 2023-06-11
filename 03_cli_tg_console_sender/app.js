992a8189ec47e41f26f4958793b219b8a159e8d7
Mykola Pankov <pankoff97@gmail.com>
2023-06-11 19:56:27 +0300

    nt fxd phtSdnr

--- 03_cli_tg_console_sender/app.js
+++ 03_cli_tg_console_sender/app.js
@@ -3,3 +3,43 @@ const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot('5615659005:AAGBL7SqnLeZsiwSWti46EnpbyGbG_8gjsg', {
   polling: false,
 });
+
+commander
+  .command('send-message <message>')
+  .description('Send a message')
+  .action((message) => {
+    const chatId = '1893906697';
+    bot
+      .sendMessage(chatId, message)
+      .then(() => {
+        console.log('Message sent successfully!');
+        process.exit();
+      })
+      .catch((error) => {
+        console.error('Error sending message:', error);
+        process.exit(1);
+      });
+  });
+
+commander
+  .command('send-photo <path>')
+  .description('Send a photo')
+  .action((path) => {
+    const chatId = '1893906697';
+    bot
+      .sendMessage(chatId, path)
+      .then(() => {
+        console.log('Photo sent successfully!');
+        process.exit();
+      })
+      .catch((error) => {
+        console.error('Error sending photo:', error);
+        process.exit(1);
+      });
+  });
+
+commander.parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+  commander.outputHelp();
+}

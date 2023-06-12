8585e9a168bd577a26c9a968103e4a7eb4659131
Mykola Pankov <pankoff97@gmail.com>
2023-06-12 18:30:39 +0300

    1st tg cnst

---
+++ 04_tg_bot_weather_4cast/bot.js
@@ -0,0 +1,7 @@
+const tgBot = require('node-telegram-bot-api');
+const axios = require('axios');
+
+const bot = new tgBot('6105075218:AAGxIDvdYSs1aWn9tN08Q8htUSHzv_mOTig', {
+  polling: true,
+});
+

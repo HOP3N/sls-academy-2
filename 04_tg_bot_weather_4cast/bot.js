dad8fe059464ef672ca1f5fe57b4b1e196e10e8d
Mykola Pankov <pankoff97@gmail.com>
2023-06-12 18:42:08 +0300

    lgc, wthr lstnrs

--- 04_tg_bot_weather_4cast/bot.js
+++ 04_tg_bot_weather_4cast/bot.js
@@ -5,3 +5,34 @@ const bot = new tgBot('6105075218:AAGxIDvdYSs1aWn9tN08Q8htUSHzv_mOTig', {
   polling: true,
 });
 
+// const chatId = '';
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const forecastOptions = {
+    reply_markup: {
+      keyboard: [
+        ['Forecast in Paris => at intervals of 3 hours'],
+        ['Forecast in Paris => at intervals of 6 hours'],
+      ],
+      one_time_keyboard: true,
+    },
+  };
+  bot.sendMessage(chatId, 'Welcome! Choose an option:', forecastOptions);
+});
+
+bot.onText(/Forecast in Paris => at intervals of (\d+) hours/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const interval = parseInt(match[1]);
+  getWeatherForecast(interval)
+    .then((forecast) => {
+      bot.sendMessage(chatId, forecast);
+    })
+    .catch((error) => {
+      console.error('Error retrieving weather forecast:', error);
+      bot.sendMessage(
+        chatId,
+        'An error occurred while retrieving the weather forecast. Please try again later.'
+      );
+    });
+});

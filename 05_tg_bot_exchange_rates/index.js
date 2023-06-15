4eda28192e0f41f835d3f3021eaf1abc5e977272
Mykola Pankov <pankoff97@gmail.com>
2023-06-15 21:37:12 +0300

    dx nw

--- 05_tg_bot_exchange_rates/index.js
+++ 05_tg_bot_exchange_rates/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const TelegramBotApi = require('node-telegram-bot-api');
-const bot = require('./bot');
+const { handleStart, handleCallbackQuery } = require('./bot');
 
 const tkn = process.env.TG_BOT_TOKEN;
 const tgBot = new TelegramBotApi(tkn, { polling: true });
 
-tgBot.onText(/\/start/, bot.handleStart);
-tgBot.on('callback_query', bot.handleCallbackQuery);
+tgBot.onText(/\/start/, handleStart);
+tgBot.on('callback_query', handleCallbackQuery);
+
+console.log('Bot is running...');

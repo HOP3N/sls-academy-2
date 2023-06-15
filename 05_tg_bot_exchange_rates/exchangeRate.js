5d8ea419c576cea5035fbaf60b6ffdc033dbfa94
Mykola Pankov <pankoff97@gmail.com>
2023-06-15 21:33:36 +0300

    xchng rt nw

--- 05_tg_bot_exchange_rates/exchangeRate.js
+++ 05_tg_bot_exchange_rates/exchangeRate.js
@@ -1,27 +1,47 @@
-const NodeCache = require('node-cache');
 const axios = require('axios');
 
-const cache = new NodeCache();
+const PRIVATBANK_API_URL =
+  'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5';
+const MONOBANK_API_URL = 'https://api.monobank.ua/bank/currency';
 
-async function getExchangeRate(currency) {
+async function getPrivatBankRate() {
   try {
-    const response = await axios.get(`API_URL/${currency}`);
-    const rate = response.data.rate;
+    const response = await axios.get(PRIVATBANK_API_URL);
+    const data = response.data;
 
-    return rate;
+    // Find the exchange rate for USD
+    const usdRate = data.find((currency) => currency.ccy === 'USD');
+    if (usdRate) {
+      return usdRate.sale;
+    }
+
+    throw new Error('Exchange rate for USD not found');
   } catch (error) {
-    throw new Error('Failed to fetch exchange rate');
+    throw new Error('Failed to fetch PrivatBank exchange rate');
   }
 }
-function getCachedRate(currency) {
-  return cache.get(currency);
-}
-function cacheRate(currency, rate) {
-  cache.set(currency, rate, CACHE_TTL);
+
+async function getMonobankRate() {
+  try {
+    const response = await axios.get(MONOBANK_API_URL);
+    const data = response.data;
+
+    // Find the exchange rate for EUR
+    const eurRate = data.find(
+      (currency) =>
+        currency.currencyCodeA === 978 && currency.currencyCodeB === 980
+    );
+    if (eurRate) {
+      return eurRate.rateBuy;
+    }
+
+    throw new Error('Exchange rate for EUR not found');
+  } catch (error) {
+    throw new Error('Failed to fetch Monobank exchange rate');
+  }
 }
 
 module.exports = {
-  getExchangeRate,
-  getCachedRate,
-  cacheRate,
+  getPrivatBankRate,
+  getMonobankRate,
 };
